940ba119cd8ffbf34291df4188a927ee5330d67e
Alberto J. Gomez <alberto.gomez@getbruin.com>
2025-07-14 18:23:24 +0200

    add new pipeline

---
+++ success_test/assets/asset_a.py
@@ -0,0 +1,23 @@
+""" @bruin
+
+name: asset_a
+
+@bruin """
+
+import os 
+import time
+
+
+print('brb, taking a quick nap')
+time.sleep(3)
+import random
+
+if random.random() < 0.5:
+    raise Exception("Heads! You lose!")
+else:
+    print("Tails! You win!")
+
+
+
+
+print("We are done here.")
---
+++ success_test/assets/asset_b.py
@@ -0,0 +1,23 @@
+""" @bruin
+
+name: asset_b
+
+@bruin """
+
+import os 
+import time
+
+
+print('brb, taking a quick nap')
+time.sleep(30)
+import random
+
+# if random.random() < 0.5:
+#     raise Exception("Heads! You lose!")
+# else:
+#     print("Tails! You win!")
+
+
+
+
+print("We are done here.")
