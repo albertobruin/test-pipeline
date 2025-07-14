ca7cb67de1acd92bc245b1c513c9cd3f1dc3d58c
Alberto J. Gomez <alberto.gomez@getbruin.com>
2025-07-14 18:44:08 +0200

    added another asset

---
+++ success_test/assets/asset_c copy.py
@@ -0,0 +1,25 @@
+""" @bruin
+
+name: asset_d
+depends:
+    - asset_c
+
+@bruin """
+
+import os 
+import time
+
+
+print('brb, taking a quick nap')
+time.sleep(3)
+import random
+
+# if random.random() < 0.5:
+#     raise Exception("Heads! You lose!")
+# else:
+#     print("Tails! You win!")
+
+
+
+
+print("We are done here.")
