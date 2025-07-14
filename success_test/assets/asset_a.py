73dd7b471de9e9e671d901583f1953a792a04759
Alberto J. Gomez <alberto.gomez@getbruin.com>
2025-07-14 18:34:02 +0200

    always fail

--- success_test/assets/asset_a.py
+++ success_test/assets/asset_a.py
@@ -12,7 +12,7 @@ print('brb, taking a quick nap')
 time.sleep(3)
 import random
 
-if random.random() < 0.5:
+if random.random() < 10.5:
     raise Exception("Heads! You lose!")
 else:
     print("Tails! You win!")
