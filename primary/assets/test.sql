031264074d7317318c9b5cfd19eeb434e8a29efb
Alberto J. Gomez <alberto.gomez@protonmail.com>
2025-10-28 13:08:54 +0100

    test

---
+++ primary/assets/test.sql
@@ -0,0 +1,19 @@
+/* @bruin
+
+name: test_data_set_us.test_table
+type: bq.source
+description: This is a test asset to check the bigquery integration
+connection: gcp-default
+
+
+
+custom_checks:
+  - name: "whatever"
+    value: 0
+    query: |-
+      SELECT DATE_DIFF(MAX(the_date), MIN(the_date), DAY) + 1 -  COUNT(DISTINCT the_date)  FROM `bruin-alberto-playground.notion.some_table` 
+
+
+@bruin */
+
+
