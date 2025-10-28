5e98728f17470e2032706601a05c58690309add7
Alberto J. Gomez <alberto.gomez@protonmail.com>
2025-10-28 13:29:24 +0100

    change scehma/name

--- primary/assets/test.sql
+++ primary/assets/test.sql
@@ -1,6 +1,6 @@
 /* @bruin
 
-name: test_data_set_us.test_table
+name: notion.some_table
 type: bq.source
 description: This is a test asset to check the bigquery integration
 connection: alberto-test-playground
@@ -11,7 +11,7 @@ custom_checks:
   - name: "whatever"
     value: 0
     query: |-
-      SELECT DATE_DIFF(MAX(the_date), MIN(the_date), DAY) + 1 -  COUNT(DISTINCT the_date)  FROM `bruin-alberto-playground.test_data_set_us.test_table` 
+      SELECT DATE_DIFF(MAX(the_date), MIN(the_date), DAY) + 1 -  COUNT(DISTINCT the_date)  FROM `bruin-alberto-playground.notion.some_table` 
 
 
 @bruin */
