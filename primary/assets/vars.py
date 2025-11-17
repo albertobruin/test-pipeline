efe64817eb5d75f6eefccffd936d31ee37b9de6e
Alberto J. Gomez <alberto.gomez@getbruin.com>
2025-11-17 22:05:32 +0100

    add vars

---
+++ primary/assets/vars.py
@@ -0,0 +1,13 @@
+""" @bruin
+
+name: vars.test 
+secrets:
+    - key: neon
+      inject_as: test_conn
+
+@bruin """
+
+
+import os, json
+vars = json.loads(os.environ["BRUIN_VARS"])
+print(vars["env"])
