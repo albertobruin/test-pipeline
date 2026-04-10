0c2b15bdcd3a36a1cad64ff2e0e999bc98c2ccf2
Bahar Okuyucu <baharokuyucu@gmail.com>
2026-04-10 12:45:46 +0300

    Add new Airflow pipeline and SQL assets

---
+++ secondary-every-minute-airflow/assets/a.sql
@@ -0,0 +1,24 @@
+/* @bruin
+
+name: public.asset_a
+type: pg.sql
+connection: neon
+
+materialization:
+    type: table
+    strategy: append
+
+custom_checks:
+  - name: silly check always passes
+    value: 1
+    query: select 1
+  - name: table row count is not null
+    value: 1
+    query: select case when count(*) is not null then 1 else 0 end from public.asset_a
+
+@bruin */
+
+SELECT
+    '{{ end_datetime }}' AS run_id,
+    'asset_a' AS asset_name,
+    '{{ execution_timestamp }}' AS inserted_at
