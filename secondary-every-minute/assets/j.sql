d94a59cf72ea2b6bc16edfbf71ca3a221e47bf82
Bahar Okuyucu <baharokuyucu@gmail.com>
2026-04-24 18:06:50 +0300

    Add dotted asset name for testing route dot handling

---
+++ secondary-every-minute/assets/j.sql
@@ -0,0 +1,17 @@
+/* @bruin
+
+name: public.dotted_test.asset_j
+type: pg.sql
+connection: neon
+
+materialization:
+    type: table
+    strategy: append
+
+
+@bruin */
+
+SELECT
+    '{{ end_datetime }}' AS run_id,
+    'dotted_test.asset_j' AS asset_name,
+    '{{ execution_timestamp }}' AS inserted_at
