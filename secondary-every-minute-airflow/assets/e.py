f37ef32fb8f6f9bb9ea4f2030befa07a7c085a9c
Bahar Okuyucu <baharokuyucu@gmail.com>
2026-05-13 14:19:48 +0300

    Remove non-blocking check from leaf asset_e, test only non-leaf asset_a

--- secondary-every-minute-airflow/assets/e.py
+++ secondary-every-minute-airflow/assets/e.py
@@ -3,12 +3,6 @@
 name: asset_e
 type: python
 
-custom_checks:
-  - name: non blocking check that always fails
-    value: 0
-    query: select 1
-    blocking: false
-
 @bruin """
 
 print("Asset E: completed successfully")
